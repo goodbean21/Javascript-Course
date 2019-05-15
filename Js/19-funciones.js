6b9193358767c50d3ae25786ba8416242122056d
jayk2 <abelca.a19@gmail.com>
2019-05-15 14:22:51 -0500

    Clase sobre funciones en Js

--- Js/19-funciones.js
+++ Js/19-funciones.js
@@ -1,6 +1,25 @@
 'use strict'
 
 /*
-  
-
+  Funciones: Es una agrupación reutilizable de un conjunto de instrucciones
 */
+
+
+// Definir la función
+function calculadora(numero1, numero2){
+  let suma = numero1 + numero2;
+  let resta = numero1 - numero2;
+  let multplicación = numero1 * numero2;
+  let división = numero1 / numero2;
+
+  console.log("Suma: " + suma);
+  console.log("Resta: " + resta);
+  console.log("multiplicar: "+ multplicación);
+  console.log("División: " + división);
+
+  return "Hola soy la calculadora";
+
+}
+
+let resultado = calculadora(1 , 2);
+console.log(resultado);
